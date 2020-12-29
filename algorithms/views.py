7ce95183f9a80f033db6c5e147849356fccda9e2
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-12-29 13:56:42 +0530

    incomplete

--- algorithms/views.py
+++ algorithms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def home(request):
+    return render(request, 'algorithms/home.html', {})
+
+def view_code(request, code_id):
+    code = Code.objects.get(id=code_id)
+    return render(request, 'algorithms/view_code.html', code)
