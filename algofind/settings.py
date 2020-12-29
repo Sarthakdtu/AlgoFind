56412013a294e2522a3ac4578fca5ba9af7a47ff
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-12-29 13:06:15 +0530

    added host

--- algofind/settings.py
+++ algofind/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's%v^-rx)h5@*t!#$gbq*=rxqhh#7!0q=-o%e%sx*$q3cj_mu*1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'algofind.pythonanywhere.com']
 
 
 # Application definition
